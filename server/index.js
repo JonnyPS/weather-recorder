fb010988b32b2c90bcd51372556e24cb5b7644f9
Jonny Spry <j.spry@arts.ac.uk>
2020-10-21 22:41:32 +0100

    setup db and add columns

--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 8080;
-const initDB = require("./database/create-db");
+// const initDB = require("./database/create-db");
+// console.log( initDB.setupDB())
+var sqlite3 = require('sqlite3').verbose();
+const DB_PATH = 'server/database/test.db'
 
-console.log( initDB.setupDB())
+const DB = new sqlite3.Database(DB_PATH, function(err){
+  if (err) {
+      console.log(err)
+      return
+  }
+  console.log('Connected to ' + DB_PATH + ' database.')
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dbSchema = `CREATE TABLE IF NOT EXISTS Users (
+  id integer NOT NULL PRIMARY KEY,
+  login text NOT NULL UNIQUE,
+  password text NOT NULL,
+  email text NOT NULL UNIQUE,
+  first_name text,
+  last_name text
+);`
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+DB.exec(dbSchema, function(err){
+if (err) {
+  console.log(err)
+}
+});
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
