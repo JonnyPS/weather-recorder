657fff08f6bde053df61ea7bb23b1a7d257bb404
Jonny Spry <j.spry@arts.ac.uk>
2020-10-23 13:35:17 +0100

    update cron schedule for server test

--- server/controllers/gets.js
+++ server/controllers/gets.js
@@ -5,7 +5,9 @@ require('dotenv').config()
 module.exports = {
   getWeather: function (database) {
     console.log('getWeather')
-    const task = cron.schedule('30 13 * * *', () => {
+    let validate = cron.validate('30 13 * * *')
+    console.log('validate = ', validate)
+    const task = cron.schedule('* * * * *', () => {
       console.log('Running CRON job now: ');
       axios.get("http://api.openweathermap.org/data/2.5/weather?q=Bristol,GB&units=metric&appid=fe46f64d11af51352d4aea674767e906")
         .then(response => {
