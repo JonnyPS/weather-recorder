c26cb2c68b13acb5eeec3b673e34b2d5be01f8e2
Jonny Spry <j.spry@arts.ac.uk>
2020-10-30 09:02:12 +0000

    update db creds

--- server/config.js
+++ server/config.js
@@ -3,10 +3,10 @@ const { Client } = require('pg');
 module.exports = {
 	setupClient: function () {
 		  client = new Client({
-			user: 'kqxmgsqidsoufa',
-			host: 'ec2-54-156-53-71.compute-1.amazonaws.com',
-			database: 'desr1fih4mehfr',
-			password: '414b00bf5ee9846341d29fe0ef8e492dc4353a76af333b401d78f9f88a361ec6',
+			user: 'jjmmmnuscdxcte',
+			host: 'ec2-3-208-224-152.compute-1.amazonaws.com',
+			database: 'dfs9na95um01n9',
+			password: '3a4373ed7b8154056ffdf78c6724d36aeeeaed381cbb8204e87cf7cb01ed8ad5',
 			port: 5432,
 		});
 		return client;
